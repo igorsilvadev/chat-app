8796b3cdbe36e12e8910a6719440562f957b44fb
Igor Silva <igorsilvadeveloper@gmail.com>
2022-10-14 20:27:19 -0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Igor Samoel da Silva on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
